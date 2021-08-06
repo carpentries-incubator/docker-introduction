1d2075d7732373f69e765c6fff00503a647b7afc
Jens <jenswehner@gmail.com>
2021-08-06 11:27:12 +0200

    Added birthday to age converter

---
+++ code/convertbirthdaytoage.py
@@ -0,0 +1,26 @@
+
+import csv
+import json
+import datetime
+import os
+
+# taken from https://stackoverflow.com/questions/2217488/age-from-birthdate-in-python
+def calculate_age(born):
+    today = datetime.date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+ages={}
+with open(r'../data/data_birthday.csv', 'rb') as f:
+    reader = csv.DictReader(f, delimiter=',',)
+    for line in reader:
+        name = line["Names"]
+        birthday = datetime.datetime.strptime(line["Birthday"],"%m/%d/%Y")
+        age= calculate_age(birthday)
+        ages[name]=age
+
+with open('ages.json', 'w') as f:
+    json.dump(ages, f)
+        
+
+
+
